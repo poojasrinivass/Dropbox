d6cf75a9f61c3ad0548fa6279f4109334c97aa14
pooja <poojasrinivas24@gmail.com>
2019-03-18 18:29:57 +0530

    Added file transafer with UDP

--- client.py
+++ client.py
@@ -4,34 +4,123 @@ import sys
 
 HOST = '127.0.0.1'
 PORT = 65432
+UDP_PORT = 9999
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 history = ""
 
+def is_json(myjson):
+  try:
+    json_object = json.loads(myjson)
+  except ValueError, e:
+    return False
+  return True
+
+def handle_output(client_socket, data, err=0):
+	if err == 1:
+		for it in data:
+			print(it)
+			return
+	client_socket.send(json.dumps("ACK"))
+	data = client_socket.recv(1024)
+
+	if not data:
+		print("No Data received!")
+		print("Exit...")
+		client_socket.close()
+		sys.exit()
+	else:
+		data = json.loads(data)
+		for it in data:
+			print(it)
+	return
+
+def handle_download_tcp(client_socket, command):
+
+	f = open(command[2], 'wb')
+
+	while True:
+		packet = client_socket.recv(1024)
+		if not packet or (is_json(packet) and json.loads(packet) == "DONE"):
+			print("File Downloaded")
+			handle_output(client_socket, packet)
+			break
+		f.write(packet)
+	f.close()
+	return
+
+def handle_download_udp(udp_socket, client_socket, command):
+	f = open(command[2], 'wb')
+
+	data, addr = udp_socket.recvfrom(1024)
+	try:
+		while(data):
+			print("Receiving...")
+			f.write(data)
+			udp_socket.settimeout(2)
+			data, addr = udp_socket.recvfrom(1024)
+	except socket.timeout:
+		f.close()
+		udp_socket.close()
+	data = client_socket.recv(1024)
+	if not data:
+		print("Error : No acknowledgement received!")
+	elif is_json(data) and json.loads(data) == "DONE":
+		print("File Downloaded")
+		handle_output(client_socket, data)
+	return
+
 while True:
+	
 	try:
 		command = raw_input("prompt=> ")
 	except KeyboardInterrupt:
 		print("Exit...")
 		client_socket.close()
 		sys.exit()
+	
 	tokens = command.split()
+	
 	if len(tokens) == 0:
 		continue
+	
 	if tokens[0] == 'history':
 		print history
 		continue
+	
 	if tokens[0] == "exit":
 		client_socket.close()
 		sys.exit()
+	
 	history += tokens[0] + "\n"
 	
 	client_socket.send(json.dumps(tokens))
 	data = client_socket.recv(1024)
+	
 	if not data:
 		print("No Data received!")
+		print("Exit...")
+		client_socket.close()
+		sys.exit()
 		continue
+	
 	data = json.loads(data)
-	for it in data:
-		print(it)
+	print(data)
+	
+	if data == "OUTPUT":
+		handle_output(client_socket, data)
+		
+	elif data == "DOWNLOAD":
+		client_socket.send(json.dumps("ACK"))
+		if tokens[1] == "TCP":
+			handle_download_tcp(client_socket, tokens)
+		else:
+			udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			udp_socket.bind((HOST, UDP_PORT))
+
+			handle_download_udp(udp_socket, client_socket, tokens)
+	else:
+		print("Error Encountered!")
+		handle_output(client_socket, data, 1)
+		
--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 import socket
-import sys
-import json
-from datetime import datetime
 import os
 import hashlib
+from datetime import datetime
+import json
+import sys
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 65432
+UDP_PORT = 9999
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(5)
 client_socket, addr = server_socket.accept()
+
+def is_json(myjson):
+  try:
+    json_object = json.loads(myjson)
+  except ValueError, e:
+    return False
+  return True
 
 def get_date(date_string):
 	tmp = date_string.split('/')
@@ -40,9 +48,7 @@ def index(commands):
 		res.append({
 			"error": "Not Enough Arguments!"
 			})
-		client_socket.send(json.dumps(res))
-		return
-	if commands[1] == "longlist":
+	elif commands[1] == "longlist":
 		ext = ""
 		if(len(commands) == 3):
 			ext = extension(commands[-1])
@@ -55,32 +61,52 @@ def index(commands):
 					})
 	elif commands[1] == "shortlist":
 
-		start_time = get_date(commands[2])
-		end_time = get_date(commands[3])
-		# print(start_time)
-		# print(end_time)
-		ext = ""
-		if(len(commands) == 5):
-			ext = extension(commands[-1])
+		if len(commands) < 4:
+			res.append({
+				"error" : "Not Enough Arguments passed!"
+				})
+		else:
+			start_time = get_date(commands[2])
+			end_time = get_date(commands[3])
+			# print(start_time)
+			# print(end_time)
+			ext = ""
+			if(len(commands) == 5):
+				ext = extension(commands[-1])
 
-		for file in file_list:
-			if ext == "" or file[-3:] == ext:
-				mtime = datetime.fromtimestamp(os.path.getmtime(file))
-				mtime = mtime.strftime('%Y-%m-%d %H:%M:%S')
-				# print(file, mtime)
-				if mtime >= start_time and mtime <= end_time:
-					res.append({
-						"name" : file,
-						"size" : os.path.getsize(file),
-						"timestamp" : mtime
-						})
+			for file in file_list:
+				if ext == "" or file[-3:] == ext:
+					mtime = datetime.fromtimestamp(os.path.getmtime(file))
+					mtime = mtime.strftime('%Y-%m-%d %H:%M:%S')
+					# print(file, mtime)
+					if mtime >= start_time and mtime <= end_time:
+						res.append({
+							"name" : file,
+							"size" : os.path.getsize(file),
+							"timestamp" : mtime
+							})
 	else:
 		res.append({
 			"error" : "index : invalid flag"
 			})
 	# print("res")
 	# print(res)
-	client_socket.send(json.dumps(res))
+	client_socket.send(json.dumps("OUTPUT"))
+	data = client_socket.recv(1024)
+	if not data:
+		res = []
+		res.append({
+			"error" : "Connection broken!"
+			})
+	data = json.loads(data)
+	if data == "ACK":
+		client_socket.send(json.dumps(res))
+	else:
+		res = []
+		res.append({
+			"Transmission not acknowledged!"
+			})
+		client_socket.send(json.dumps(res))
 	return
 
 def filehash(commands):
@@ -90,10 +116,8 @@ def filehash(commands):
 		res.append({
 			"error": "Not Enough Arguments!"
 			})
-		client_socket.send(json.dumps(res))
-		return
 
-	if commands[1] == "verify":
+	elif commands[1] == "verify":
 		if len(commands) != 3:
 			res.append({
 				"error": "Improper Arguments passed!"
@@ -106,11 +130,13 @@ def filehash(commands):
 					"error" : "File does not exist!"
 					})
 			else:
-				res.append({
-					"name" : file,
-					"checksum" : md5(file),
-					"timestamp" : datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
-					})
+				if os.path.isfile(file):
+
+					res.append({
+						"name" : file,
+						"checksum" : md5(file),
+						"timestamp" : datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
+						})
 	elif commands[1] == "checkall":
 		file_list = os.listdir(".")
 		for file in file_list:
@@ -124,22 +150,132 @@ def filehash(commands):
 		res.append({
 			"error" : "index : invalid flag"
 			})
-	client_socket.send(json.dumps(res))
+	
+	client_socket.send(json.dumps("OUTPUT"))
+	data = client_socket.recv(1024)
+	if not data:
+		res = []
+		res.append({
+			"error" : "Connection broken!"
+			})
+	data = json.loads(data)
+	if data == "ACK":
+		client_socket.send(json.dumps(res))
+	else:
+		res = []
+		res.append({
+			"Transmission not acknowledged!"
+			})
+		client_socket.send(json.dumps(res))
 	return
 
+
+def download(commands):
+	res = []
+	client_socket.send(json.dumps("DOWNLOAD"))
+	data = client_socket.recv(1024)
+	if not data:
+		print("Error : Connection broken!")
+		return
+	elif is_json(data) and json.loads(data) != "ACK":
+		res.append({
+			"error" : "No acknowledgement received!"
+			})
+	else:
+		if len(commands) < 3:
+			res.append({
+				"error": "Not Enough Arguments!"
+				})
+			client_socket.send(json.dumps(res))
+			return
+		elif commands[1] == "TCP":
+			file = commands[2]
+			if not os.path.isfile(file):
+				res.append({
+					"error" : "File does not exist!"
+					})
+				client_socket.send(json.dumps(res))
+				return
+			else:
+				data = client_socket.recv(1024)
+				f = open(file, 'rb')
+				packet = f.read(1024)
+				while packet:
+					print("Sending..")
+					client_socket.send(packet)
+					packet = f.read(1024)
+				f.close()
+				client_socket.send(json.dumps("DONE"))
+				res.append({
+					"name" : file,
+					"size" : os.path.getsize(file),
+					"checksum" : md5(file),
+					"timestamp" : datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
+					})
+				data = client_socket.recv(1024)
+				if not data or json.loads(data) != "ACK":
+					res = []
+					res.append({
+						"error" : "No acknowledgement received!"
+						})
+				client_socket.send(json.dumps(res))
+		elif commands[1] == "UDP":
+			udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			dest = (HOST, UDP_PORT)
+
+			res = []
+			file = commands[2]
+			if not os.path.isfile(file):
+				res.append({
+					"error" : "File does not exist!"
+					})
+				udp_socket.sendto(res, dest)
+				return
+			else:
+				f = open(file, 'rb')
+				packet = f.read(1024)
+				while packet:
+					print("Sending..")
+					udp_socket.sendto(packet, dest)
+					packet = f.read(1024)
+				udp_socket.close()
+				f.close()
+				res.append({
+					"name" : file,
+					"size" : os.path.getsize(file),
+					"checksum" : md5(file),
+					"timestamp" : datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
+					})
+				client_socket.send(json.dumps("DONE"))
+				data = client_socket.recv(1024)
+				if not data:
+					print("Error : Connection broken!")
+					return
+				elif is_json(data) and json.loads(data) == "ACK":
+					client_socket.send(json.dumps(res))
+				else:
+					res = []
+					res.append({
+						"error" : "No acknowledgement received!"
+						})
+					client_socket.send(json.dumps(res))
+	return
 
 def process(commands):
 	if commands[0] == "index":
 		index(commands)
 	elif commands[0] == "filehash":
 		filehash(commands)
+	elif commands[0] == "download":
+		download(commands)
 
 while True:
 	data = client_socket.recv(1024)
 	if not data:
 		break
 	data = json.loads(data)
-	print("DATA = ", data)
+	if data == "ACK":
+		continue
 	try:
 		process(data)
 	except Exception as e:
